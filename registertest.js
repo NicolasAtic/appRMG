bb488ebbf05568738aa8f2a4fba4ca01bcd0fb39
JAQUELINE CLAUDIA BARRIONUEVO FLORES <jaquelineclaudia.barrionuevo@alu.uclm.es>
2024-06-19 17:49:34 +0200

    json next

--- registertest.js
+++ registertest.js
@@ -1,10 +1,24 @@
-const processall = (event) => {
+// Escucha el evento de envío del formulario
+const form = document.querySelector('#registrationForm');
+form.addEventListener('submit', processall);
+
+function processall(event) {
     event.preventDefault();
+
+    // Obtén los datos del formulario
     const datos = new FormData(event.target);
     const datosCompletos = Object.fromEntries(datos.entries());
-    console.log(JSON.stringify(datosCompletos));
 
-    // Redirige al usuario a la siguiente página
-    window.location.href = 'siguiente_pagina.html';
+    // Convierte los datos a formato JSON
+    const json_data = JSON.stringify(datosCompletos);
+
+    // Almacena los datos en el almacenamiento local
+    localStorage.setItem('form_data', json_data);
+
+    // show data
+    console.log('Datos guardados en formato JSON:');
+    console.log(json_data);
+
+    // brings the other page
+   window.location.href = '3RAval.html';
 }
-form.addEventListener('submit', processall);
