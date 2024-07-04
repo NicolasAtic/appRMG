2df5e789807ab9cc5e8ca6b5757b211e8ec74ec1
JAQUELINE CLAUDIA BARRIONUEVO FLORES <jaquelineclaudia.barrionuevo@alu.uclm.es>
2024-07-04 17:18:56 +0200

    clean just doc still

--- js/borrador.js
+++ js/borrador.js
@@ -80,6 +80,41 @@ https://script.google.com/macros/library/d/1YKhxV_8OA5JGU9zbCXqw0dhO_Ar8Sw-rmvWe
     </select>
     <input type="text" class="form-control" id="docNumber" name="docNumber" placeholder="número de documento">
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    const form = document.getElementById('registrationForm');
+        
+        const processAll = (event) => {
+        event.preventDefault();
+        const datos = new FormData(event.target);
+        const datosCompletos = Object.fromEntries(datos.entries());
+        console.log(JSON.stringify(datosCompletos));
+
+ // Enviar datos a Google Apps Script
+    fetch('https://script.google.com/macros/s/AKfycbzB53_MUylQL06XHNzNovn2da-VnAYnXFkGsrEEffNDvkWWfL8XfdnodoJHBk7yHFTw/exec', {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json'
+            },
+        body: JSON.stringify(datosCompletos)
+    })
+        .then(data => {
+        console.log('Success:',data); // esta funcion no esta bien debo revisar sgit
+                    // Redirige al usuario a la siguiente página
+            setTimeout(() => {
+            window.location.href = '3RAval.html';
+            }, 100); // Retrasa la redirección para asegurarse de que el JSON se muestra en la consola
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    };
+
+form.addEventListener('submit', processAll);
+});
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
