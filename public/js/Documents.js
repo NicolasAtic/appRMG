52667d72bf42f18e841316d7c12ffbe7d0b3a885
Diditoleu <113026515+Diditoleu@users.noreply.github.com>
2024-07-16 12:39:04 +0200

    ccc

--- public/js/Documents.js
+++ public/js/Documents.js
@@ -1,33 +1,36 @@
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
-import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
-import { db, storage } from './firebase.js';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
+import { db } from './firebase.js';
 
 const auth = getAuth();
+const storage = getStorage(); // en esta carpeta se guardaran los archivos con el uid 
 const logOutBtn = document.getElementById("logout-btn");
 const UIuserEmail = document.getElementById("user-email");
 const RdocumentForm = document.getElementById("document-form");
 
+// this is for take it back data from user that was saved snap 
 const loadUserData = async (user) => {
     const docRef = doc(db, "users", user.uid);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
         const userData = docSnap.data();
-        UIuserEmail.innerHTML = userData.email;
+        UIuserEmail.innerHTML = userData.email; // i user has data comeback to user frontend
     } else {
-        console.log("No such document!");
+        console.log("No such document!"); // if user do not have data come cabk this
     }
 };
-
+// if user is not login takes direct to the login page 
 onAuthStateChanged(auth, (user) => {
     if (user) {
         loadUserData(user);
     } else {
-        window.location.href = '3Login.html';
+        window.location.href = '1Login.html';
     }
 });
 
+// the documents go direct to firestorage with user id and docnamen 
 const uploadDocument = async (file, userId, docName) => {
     const storageRef = ref(storage, `${userId}/${docName}`);
     await uploadBytes(storageRef, file);
@@ -35,12 +38,13 @@ const uploadDocument = async (file, userId, docName) => {
     return downloadURL;
 };
 
+// take url of documents and go to clous firestorage 
 const saveDocumentURLs = async (user, urls) => {
     await setDoc(doc(db, "users", user.uid), {
-        documentURLs: urls
+        udocumentURLs: urls
     }, { merge: true });
 };
-
+//  now working  submit doc 
 RdocumentForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const user = auth.currentUser;
@@ -48,7 +52,7 @@ RdocumentForm.addEventListener("submit", async (e) => {
     const DNIAval = document.getElementById("2.DNIAval").files[0];
     const Nominas = document.getElementById("3.Nominas").files[0];
     const Ucarta = document.getElementById("4.Cartauniversidad").files[0];
-
+// takes url and save in good way
     const urls = {};
 
     if (DNIinquilino) urls.DNIinquilino = await uploadDocument(DNIinquilino, user.uid, "1.DNIinquilino");
@@ -58,13 +62,16 @@ RdocumentForm.addEventListener("submit", async (e) => {
 
     await saveDocumentURLs(user, urls);
     alert("Documents uploaded successfully!");
-    window.location.href = '7Final.html';
+    // fo next page if works
+    window.location.href = '.Final.html';
 });
 
+// press log out data save to 
 const logOutButtonPressed = async () => {
     try {
+        const user = auth.currentUser;
         await signOut(auth);
-        window.location.href = '3Login.html';
+        window.location.href = '1Login.html';
     } catch (error) {
         console.log(error);
     }
