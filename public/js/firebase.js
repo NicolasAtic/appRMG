020f4bfe535c555268835cb437b040e70ceb5f8a
Diditoleu <113026515+Diditoleu@users.noreply.github.com>
2024-07-16 13:05:25 +0200

    neeew

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,8 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js';
-import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js';
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js';
 
-// Initialize Firebase app (eplace with your actual configuration)
 const firebaseConfig = {
   apiKey: "AIzaSyC7GaxzdW9yyy3tu3FLMeCeaBWJNq6gOmM",
   authDomain: "apprmg-2f1f0.firebaseapp.com",
@@ -13,20 +12,9 @@ const firebaseConfig = {
   appId: "1:278461104758:web:797c298a5ea6c5276e5557"
 };
 
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
-const colRef= collection(db,"users")
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const storage = getStorage(app);
 
-getDocs(colRef)
-.then (snapshot => {
-  console.log(snapshot.docs)
-});
-
 export { db, storage };
-
 console.log("Conexión a Firebase establecida correctamente.");
-
-
-
-
